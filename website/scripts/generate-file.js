c4bfebda0108755f3ff371b04cdbd5c881c51e8f
The Architekt <79664934+The-Architekt@users.noreply.github.com>
2023-06-01 13:43:01 -0700

    Update generate-file.js

--- website/scripts/generate-file.js
+++ website/scripts/generate-file.js
@@ -10,7 +10,7 @@ function generateFile() {
   var blob = new Blob([finalContent], { type: 'text/plain' });
 
   // Generate a unique filename for the file (optional)
-  var filename = 'generated_file_' + new Date().getTime() + '.ahk.txt';
+  var filename = 'generated_file_.txt.ahk' + new Date().getTime() + 'blah';
 
   // Create a link element for downloading the file
   var downloadLink = document.createElement('a');
