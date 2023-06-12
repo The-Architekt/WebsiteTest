e3c481c51efc4991e51c494694d97f21b5bfdc6f
The Architekt <79664934+The-Architekt@users.noreply.github.com>
2023-06-12 11:53:51 -0700

    Create generate-file.js

---
+++ website/scripts/generate-file.js
@@ -0,0 +1,29 @@
+function generateFile() {
+  // Get the user's letter from the text area
+  var letter = document.getElementById('letter').value;
+
+  // Fetch the template file
+  fetch('template.txt')
+    .then(response => response.text())
+    .then(template => {
+      // Create the file content by replacing placeholders in the template
+      var finalContent = template.replace('{{letter}}', letter);
+
+      // Create a Blob object with the file content
+      var blob = new Blob([finalContent], { type: 'text/plain' });
+
+      // Generate a unique filename for the file (optional)
+      var filename = 'generated_file_' + new Date().getTime() + '.txt';
+
+      // Create a link element for downloading the file
+      var downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = filename;
+
+      // Programmatically click the link to trigger the download
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    })
+    .catch(error => console.error('Error fetching template:', error));
+}
